b2f0edc6a3ee072d4ea23af841d88096bfc96079
Devin Bess <dbess004@fiu.edu>
2024-12-06 13:17:31 -0500

    Created Sequence Login program

---
+++ SequenceLogin.py
@@ -0,0 +1,36 @@
+# Simple Login System: Two Passwords Required
+# Sequence Login
+
+# User credentials
+user_db = {
+    # "user1": ["pass1", "pass1"], # Multi-attempt login is defined as using two identical passwords
+    "user2": ["hello1", "world2"], # Sequence login is defined as using two different passwords
+}
+
+errM = "Username or password are incorrect."
+prompt = "Enter Password: "
+
+while True:
+    print("Attempting Login...\n")
+    
+    username = input("Username: ").strip()
+    
+    # Validate username
+    if username not in user_db:
+        print(errM)
+        continue
+    
+    # Validate first password
+    password1 = input(prompt)
+    if password1 != user_db[username][0]:
+        print(errM)
+        continue
+    
+    # Validate second password
+    password2 = input(prompt)
+    if password2 != user_db[username][1]:
+        print(errM)
+        continue
+    
+    print(f"Login successful! Welcome, {username}.\n")
+    break
